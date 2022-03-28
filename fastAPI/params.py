0cfdac3ccc450547616285d360df05f2122e1686
Mathieu <mathieu.surman@gmail.com>
2022-03-28 10:34:02 +0200

    fastAPI IMdb commencement

---
+++ fastAPI/params.py
@@ -0,0 +1,14 @@
+site = "https://imdb-api.com/API/"
+api_key = "k_pp1u5n65/"
+
+research_dico ={
+    "STitle":"SearchTitle/",
+    "SMovie":"SearchMovie/",
+    "SSeries":"SearchSeries/",
+    "SName":"SearchName/",
+    "SAll":"SearchAll/",
+    "TopMovies":"Top250Movies/",
+    "TopSeries":"Top250TVs/",
+    "MsotPopMovies":"MostPopularMovies/",
+    "MostPopSeries":"MostPopularTVs/"
+}
