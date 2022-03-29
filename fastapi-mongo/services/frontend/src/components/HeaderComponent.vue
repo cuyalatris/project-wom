1220863cabe5c56fa0f8dabc98c7333304bdb47d
cuyalatris <cuyalatris@cy-tech.fr>
2022-03-29 12:39:01 +0200

    Copie vue_js -> frontend 3

---
+++ fastapi-mongo/services/frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>WatchOurMovie</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    div {
+        padding: 10px;
+        color: #ffffff;
+        background-color: #2c3e50;
+    }
+</style>
