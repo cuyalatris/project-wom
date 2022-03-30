19157eb3f949dd44af4674d0674e1b41682948d4
cuyalatris <cuyalatris@cy-tech.fr>
2022-03-30 10:44:04 +0200

    Front form inscription

--- fastapi-mongo/services/frontend/src/components/InscriptionComponent.vue
+++ fastapi-mongo/services/frontend/src/components/InscriptionComponent.vue
@@ -1,28 +1,43 @@
 <template>
+    <div id="inscriptionTitle">
+        <h2>Sign in</h2>
+    </div>
     <div id="inscriptionForm">
         <ui-form type="|" item-margin-bottom="16" action-align="center">
             <template #default="{ actionClass }">
                 <ui-form-field>
-                    <ui-textfield outlined input-type="email" required>
+                    <ui-textfield outlined id="Username" v-model="valueUsername" input-type="text" pattern=".{4,}" required>
+                        Username
+                    </ui-textfield>
+                </ui-form-field>
+                <ui-form-field>
+                    <ui-textfield outlined id="firstEmail" v-model="valueEmail1" input-type="email" required>
                         Email
                     </ui-textfield>
                 </ui-form-field>
                 <ui-form-field>
-                    <ui-textfield input-type="email" required>
-                        Confirmer l'email
+                    <ui-textfield outlined id="secondEmail" v-model="valueEmail2" input-type="email" required>
+                        Confirm email
                     </ui-textfield>
                 </ui-form-field>
                 <ui-form-field>
-                    <ui-textfield outlined input-type="password" required pattern=".{8,}" helper-text-id="pw-validation-msg" :attrs="{autocomplete: 'current-password'}">
+                    <ui-textfield outlined id="firstPassword" v-model="valuePassword1" input-type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required>
                         Password
                     </ui-textfield>
-                    <ui-textfield-helper id="pw-validation-msg" visible>
-                        Must be at least 8 characters long
+                </ui-form-field>
+                <ui-form-field>
+                    <ui-textfield outlined id="secondPassword" v-model="valuePassword2" input-type="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" required>
+                        Confirm password
+                    </ui-textfield>
+                </ui-form-field>
+                <ui-form-field>
+                    <ui-textfield-helper id="password-helper" visible>
+                        Password must be at least 8 characters<br>long, at least 1 uppercase letter,<br> 1 lowercase letter<br> and 1 number
                     </ui-textfield-helper>
                 </ui-form-field>
                 <ui-form-field :class="actionClass">
-                    <ui-button raised>Submit</ui-button>
-                    <ui-button outlined>Cancel</ui-button>
+                    <p hidden>{{ enableSubmit() }}</p>
+                    <ui-button :disabled="isDisabled" raised>Sign In</ui-button>
                 </ui-form-field>
             </template>
         </ui-form>
@@ -30,8 +45,57 @@
 </template>
 
 <script>
-
+export default {
+    data() {
+        return {
+            valueEmail1: '',
+            valueEmail2: '',
+            valuePassword1: '',
+            valuePassword2: '',
+            isDisabled: true
+        }
+    },
+    methods: {
+        checkSameEmail() {
+            return (this.validateEmail() && this.valueEmail1 == this.valueEmail2)
+        },
+        checkSamePassword() {
+            return this.valuePassword1 == this.valuePassword2
+        },
+        enableSubmit() {
+            if (this.checkSameEmail() && this.checkSamePassword()) {
+                this.isDisabled = false
+            } else {
+                this.isDisabled = true
+            }
+        },
+        validateEmail() {
+            if (/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/.test(this.valueEmail1)) {
+                return true
+            } else {
+                return false
+            }
+        },
+        validatePassword() {
+            if (/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(this.valuePassword1)) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
 </script>
 
 <style scoped lang="scss">
+#inscriptionTitle {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#inscriptionForm {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
