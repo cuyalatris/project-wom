a7d5ef795e0bb5fa15e8d018bdbfa676d65dcb78
cuyalatris <cuyalatris@cy-tech.fr>
2022-03-31 15:29:14 +0200

    Ajout éléments dans Header

--- fastapi-mongo/services/frontend/src/components/HeaderComponent.vue
+++ fastapi-mongo/services/frontend/src/components/HeaderComponent.vue
@@ -1,13 +1,32 @@
 <template>
     <div>
-        <h1>WatchOurMovie</h1>
+        <ui-grid>
+            <ui-grid-cell id="headerCellTitle">
+                <h1>WatchOurMovie</h1>
+            </ui-grid-cell>
+            <ui-grid-cell id="headerCellSearchBar">
+                <ui-textfield id="searchBar" v-model="searchBarInput" :source="moviesList" placeholder="Search for a movie"></ui-textfield>
+            </ui-grid-cell>
+            <ui-grid-cell class="heacerCell"></ui-grid-cell>
+        </ui-grid>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            searchBarInput: '',
+            moviesList: ["Movie1","Movie2","Movie3","Movie4","Movie5","Movie6","Movie7"]
+        }
+    }
+}
+</script>
 
 <style scoped lang="scss">
     div {
         padding: 10px;
-        color: #ffffff;
-        background-color: #2c3e50;
+        color: #fff;
+        background-color: #2c3e50
     }
 </style>
