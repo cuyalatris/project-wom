5007f0286987e33310c052152d92a3d6e5939fcc
Mathieu <mathieu.surman@gmail.com>
2022-04-01 16:48:00 +0200

    affichage des films sur l'acceuil qui ne marche pas

---
+++ fastapi-mongo/services/frontend/src/components/AccueilComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>   
+        <ui-grid class="gridou" position="center">
+            <ui-grid-cell v-for="i in movies" :key="i" class="demo-cell" columns="2">
+                <div>
+                    <span>{{i.id}}</span>
+                </div>
+            </ui-grid-cell>
+        </ui-grid>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+axios.defaults.baseURL = 'http://0.0.0.0:5000/'
+
+export default {
+    data() {
+        return {
+            movies:'',
+        }
+    },
+    methods: {
+        GetMovies() {
+            axios.get("/movie/")
+            .then((response) => {this.movies = response.data})
+            //.catch(e => console.log(e))
+            console.log(this.movies)
+        }
+    },
+    mounted() {
+        this.GetMovies()
+    },
+}
+</script>
--- fastapi-mongo/services/frontend/src/views/AccueilView.vue
+++ fastapi-mongo/services/frontend/src/views/AccueilView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <h1>Accueil</h1>
+    <AccueilComponent/>
   </div>
 </template>
 
 <script>
+import AccueilComponent from "@/components/AccueilComponent.vue"
 export default {
-  inject: ['loggedin'],
-  methods: {
-    checkLoggedIn() {
-      console.log(this.loggedin)
-    }
-  }
+    inject: ["loggedin"],
+    methods: {
+        checkLoggedIn() {
+            console.log(this.loggedin);
+        }
+    },
+    components: { AccueilComponent }
 }
 </script>
